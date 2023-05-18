faa511efd0fb9be7933bedbf419503535f2f9ad7
Adam Killander <34036252+Adamkillander96@users.noreply.github.com>
2023-05-18 11:08:47 +0200

    chore: I am stupid

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
-import htmlPurge from 'vite-plugin-html-purgecss'
+//import htmlPurge from 'vite-plugin-html-purgecss'
 
 // https:/vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), htmlPurge()]
+  plugins: [vue()]
 })
